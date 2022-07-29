34e9fbff7fdbb4f38b9fd756946568487959e765
aymen <aymen.shabou@gmail.com>
2022-07-30 00:43:24 +0200

    adding my first book!

---
+++ _build/html/_sources/concept2.md
@@ -0,0 +1,5 @@
+# concept 2
+
+le concept 2 est fils du [concept3]
+
+[concept3]: ./concept3.md "parent"
---
+++ _build/jupyter_execute/concept1_nb.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 1
+
+# In[1]:
+
+
+markd = 'concept1.md'
+
+
+# In[2]:
+
+
+from utils import find_recursive_parents
+import networkx as nx
+import matplotlib.pyplot as plt
+
+list_paires=[]
+parents = find_recursive_parents(markd, list_paires)     
+#print (parents)
+G = nx.DiGraph()
+G.add_edges_from(parents, label="parent")
+nx.draw(G, with_labels=True)
+plt.show()
+
+
+# ```{include} ./concept1.md
+# ```
